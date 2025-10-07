a78fef3fd078535c31340ad08291a4f44152df3f
tatyanazayseva88-alt <tatyanazayseva88@gmail.com>
2025-10-07 22:12:54 +0500

    hello2

--- src/constants/backend.constants.ts
+++ src/constants/backend.constants.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://rental-orm-backend-1.onrender.com'
-export const API_URL = 'http://localhost:3000'
+export const API_URL = 'https://rental-orm-backend-1.onrender.com'
+// export const API_URL = 'http://localhost:3000'
